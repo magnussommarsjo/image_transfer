bcafe950a5cc02a7824d88c19ca87e91bb231a11
Magnus Sommarsjö <magnus.lengquist@gmail.com>
2023-02-19 16:33:08 +0100

    Add progress bar and move command

--- src/imgtrf/__main__.py
+++ src/imgtrf/__main__.py
@@ -1,29 +1,45 @@
 import typer
-from imgtrf.core import copy_files
+from imgtrf.core import copy_files, move_files
 from pathlib import Path
+import logging
+
+log = logging.getLogger()
 
 app = typer.Typer()
 
 
 @app.command()
-def copy(src_dir: str, dest_dir: str):
+def copy(src_dir: str, dest_dir: str, verbose: bool = False):
     """Copy files from source dir to destination dir"""
 
     source_path = Path(src_dir).resolve()
     destination_path = Path(dest_dir).resolve()
 
+    if verbose:
+        log.setLevel("INFO")
+
     if not source_path.exists() or not source_path.is_dir():
         print("Source directory does not exists")
         raise NotADirectoryError(src_dir)
 
-    print("Copying files...")
     copy_files(src_dir=source_path, dest_dir=destination_path)
-    print("...finished copying files")
+
 
 @app.command()
-def move(src_dir: str, dest_dir: str):
+def move(src_dir: str, dest_dir: str, verbose: bool = False):
     """Move files from source dir to destination dir"""
-    print("Moving files")
+
+    source_path = Path(src_dir).resolve()
+    destination_path = Path(dest_dir).resolve()
+
+    if verbose:
+        log.setLevel("INFO")
+
+    if not source_path.exists() or not source_path.is_dir():
+        print("Source directory does not exists")
+        raise NotADirectoryError(src_dir)
+
+    move_files(src_dir=source_path, dest_dir=destination_path)
 
 
 app()
--- src/imgtrf/core.py
+++ src/imgtrf/core.py
@@ -3,8 +3,9 @@ from pathlib import Path
 from typing import Iterator
 import shutil
 import datetime
-
 import logging
+
+from rich import progress
 
 log = logging.getLogger()
 
@@ -52,19 +53,18 @@ def create_path_based_on_creation_date(
             )
         case DateDepth.DAY:
             target_path = dest_dir.joinpath(
-                str(date.year), f"{date.month:02d}", f"{date.day:02d}", src_file_path.name
+                str(date.year),
+                f"{date.month:02d}",
+                f"{date.day:02d}",
+                src_file_path.name,
             )
 
     return target_path
 
 
-def copy_files(
-    src_dir: Path,
-    dest_dir: Path,
-    date_depth: DateDepth = DateDepth.DAY,
-    skip_existing=True,
-) -> None:
-    """Copies files from source to target directory"""
+def _create_src_dest_pairs(
+    src_dir: Path, dest_dir: Path, date_depth: DateDepth, skip_existing: bool = True
+) -> list[tuple[str, str]]:
     src_dest_paths: list[tuple[str, str]] = []
     for src_file_path in walk(src_dir):
         target_path = create_path_based_on_creation_date(
@@ -75,7 +75,50 @@ def copy_files(
             continue
         else:
             src_dest_paths.append((src_file_path, target_path))
+    return src_dest_paths
 
-    for src_file_path, target_path in src_dest_paths:
+
+def copy_files(
+    src_dir: Path,
+    dest_dir: Path,
+    date_depth: DateDepth = DateDepth.DAY,
+    skip_existing=True,
+) -> None:
+    """Copies files from source to target directory"""
+    src_dest_paths = _create_src_dest_pairs(
+        src_dir, dest_dir, date_depth, skip_existing
+    )
+
+    if not src_dest_paths:
+        print("No files to copy")
+        log.info("No files to copy")
+        return
+
+    for src_file_path, target_path in progress.track(
+        src_dest_paths, description="Copying files"
+    ):
         log.info(f"Copying {src_file_path} to {target_path}")
         _copy_file(src_file_path, target_path)
+
+
+def move_files(
+    src_dir: Path,
+    dest_dir: Path,
+    date_depth: DateDepth = DateDepth.DAY,
+    skip_existing=True,
+) -> None:
+    """Copies files from source to target directory"""
+    src_dest_paths = _create_src_dest_pairs(
+        src_dir, dest_dir, date_depth, skip_existing
+    )
+
+    if not src_dest_paths:
+        print("No files to move")
+        log.info("No files to move")
+        return
+
+    for src_file_path, target_path in progress.track(
+        src_dest_paths, description="Moving files"
+    ):
+        log.info(f"Moving {src_file_path} to {target_path}")
+        _move_file(src_file_path, target_path)
