c2e47b04c25796f78ad870203a207d95df9738b9
Magnus Sommarsjö <magnus.lengquist@gmail.com>
2023-02-19 16:48:07 +0100

    Add date-level flag

--- src/imgtrf/__main__.py
+++ src/imgtrf/__main__.py
@@ -1,5 +1,5 @@
 import typer
-from imgtrf.core import copy_files, move_files
+from imgtrf.core import copy_files, move_files, DateDepth
 from pathlib import Path
 import logging
 
@@ -7,11 +7,23 @@ log = logging.getLogger()
 
 app = typer.Typer()
 
+def parse_date_depth(date_depth: str) -> DateDepth:
+    match date_depth.lower():
+        case 'day':
+            return DateDepth.DAY
+        case 'month':
+            return DateDepth.MONTH
+        case 'year':
+            return DateDepth.YEAR
+        case _:
+            raise ValueError(f"Argument {date_depth=} not recognised")
 
 @app.command()
-def copy(src_dir: str, dest_dir: str, verbose: bool = False):
+def copy(src_dir: str, dest_dir: str, date_level: str = 'day', verbose: bool = False):
     """Copy files from source dir to destination dir"""
 
+    date_depth = parse_date_depth(date_level)
+
     source_path = Path(src_dir).resolve()
     destination_path = Path(dest_dir).resolve()
 
@@ -22,13 +34,15 @@ def copy(src_dir: str, dest_dir: str, verbose: bool = False):
         print("Source directory does not exists")
         raise NotADirectoryError(src_dir)
 
-    copy_files(src_dir=source_path, dest_dir=destination_path)
+    copy_files(src_dir=source_path, dest_dir=destination_path, date_depth=date_depth)
 
 
 @app.command()
-def move(src_dir: str, dest_dir: str, verbose: bool = False):
+def move(src_dir: str, dest_dir: str, date_level: str = 'day', verbose: bool = False):
     """Move files from source dir to destination dir"""
-
+    
+    date_depth = parse_date_depth(date_level)
+    
     source_path = Path(src_dir).resolve()
     destination_path = Path(dest_dir).resolve()
 
@@ -39,7 +53,7 @@ def move(src_dir: str, dest_dir: str, verbose: bool = False):
         print("Source directory does not exists")
         raise NotADirectoryError(src_dir)
 
-    move_files(src_dir=source_path, dest_dir=destination_path)
+    move_files(src_dir=source_path, dest_dir=destination_path, date_depth=date_depth)
 
 
 app()
