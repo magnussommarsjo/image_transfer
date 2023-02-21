fa624fa2d71ccc249e04061c37b7262278f9557e
Magnus Sommarsjö <magnus.lengquist@gmail.com>
2023-02-21 12:39:49 +0100

    Fix verbosity
    Added working verbosity through logging module.
    - verbose => INFO
    - debug => DEBUG

--- src/imgtrf/cli.py
+++ src/imgtrf/cli.py
@@ -3,23 +3,37 @@ from imgtrf.core import copy_files, move_files, DateDepth
 from pathlib import Path
 import logging
 
-log = logging.getLogger()
+from rich import print
+
+from imgtrf.logger import root_logger as log
+
+# Set up logging
+stream_handler = logging.StreamHandler()
+log.addHandler(stream_handler)
 
 app = typer.Typer()
 
+
 def parse_date_depth(date_depth: str) -> DateDepth:
     match date_depth.lower():
-        case 'day':
+        case "day":
             return DateDepth.DAY
-        case 'month':
+        case "month":
             return DateDepth.MONTH
-        case 'year':
+        case "year":
             return DateDepth.YEAR
         case _:
             raise ValueError(f"Argument {date_depth=} not recognised")
 
+
 @app.command()
-def copy(src_dir: str, dest_dir: str, date_level: str = 'day', verbose: bool = False):
+def copy(
+    src_dir: str,
+    dest_dir: str,
+    date_level: str = "day",
+    verbose: bool = False,
+    debug: bool = False,
+):
     """Copy files from source dir to destination dir"""
 
     date_depth = parse_date_depth(date_level)
@@ -27,8 +41,7 @@ def copy(src_dir: str, dest_dir: str, date_level: str = 'day', verbose: bool = F
     source_path = Path(src_dir).resolve()
     destination_path = Path(dest_dir).resolve()
 
-    if verbose:
-        log.setLevel("INFO")
+    _set_log_level(verbose, debug)
 
     if not source_path.exists() or not source_path.is_dir():
         print("Source directory does not exists")
@@ -38,19 +51,32 @@ def copy(src_dir: str, dest_dir: str, date_level: str = 'day', verbose: bool = F
 
 
 @app.command()
-def move(src_dir: str, dest_dir: str, date_level: str = 'day', verbose: bool = False):
+def move(
+    src_dir: str,
+    dest_dir: str,
+    date_level: str = "day",
+    verbose: bool = False,
+    debug: bool = False,
+):
     """Move files from source dir to destination dir"""
-    
+
     date_depth = parse_date_depth(date_level)
-    
+
     source_path = Path(src_dir).resolve()
     destination_path = Path(dest_dir).resolve()
 
-    if verbose:
-        log.setLevel("INFO")
+    _set_log_level(verbose, debug)
 
     if not source_path.exists() or not source_path.is_dir():
         print("Source directory does not exists")
         raise NotADirectoryError(src_dir)
 
     move_files(src_dir=source_path, dest_dir=destination_path, date_depth=date_depth)
+
+
+def _set_log_level(verbose: bool = False, debug: bool = False):
+    if verbose:
+        log.setLevel(logging.INFO)
+
+    if debug:
+        log.setLevel(logging.DEBUG)
